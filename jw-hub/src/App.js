d022d0335f30376a4ee2998baf936cc2d94ba557
Tanner Helton <tannerhelton@Tanners-MacBook-Pro.local>
2020-01-30 14:56:21 -0600

    First functional component

--- jw-hub/src/App.js
+++ jw-hub/src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Intro(props) {
+  return (
+    <p className="App-intro">
+      Our first functional component
+    </p>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mr. Clank is big dumb
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">TV Series List</h1>
       </header>
+      <Intro />
     </div>
   );
 }
